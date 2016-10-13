be3359ed5f96c8870c2233ffbc6497cb59882ad4
gollakousik123 <golla.kousik@gmail.com>
2016-10-13 14:16:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName',function(req,res){
  var articleName=req.params.articleName;
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
+    res.send(createTemplate(articles[articleName]));
 });
  
 
