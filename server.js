7e80c12fa75317db6dcd4548c3ff398385e919f9
gollakousik123 <golla.kousik@gmail.com>
2016-12-09 11:57:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,6 +65,10 @@ var htmlTemplate = `
     <meta name="viewport" content="width=device-width,initial-scale=1"/>
     
      <link href="/ui/style.css" rel="stylesheet" />
+      <meta charset="utf-8"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  <link href='https://fonts.googleapis.com/css?family=Roboto:300,400,700' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="main.css">
     </head>
         <body>
             <div class="container">
