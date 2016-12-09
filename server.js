32172f788311cf887f1072350202ba942eea41a6
gollakousik123 <golla.kousik@gmail.com>
2016-12-09 12:55:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ var htmlTemplate = `
 
 </body>
 </html>
-
+`
 ;
 return htmlTemplate;
 }
@@ -123,5 +123,5 @@ app.get('/ui/madi.png', function (req, res) {
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+    
 });
