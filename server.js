cda30df87e88cd0530e6066d3885129455683a31
gollakousik123 <golla.kousik@gmail.com>
2016-12-10 14:03:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ var htmlTemplate = `
 <section>
     <div class="row">
         <h3 id="gender">gender</h3>
-        <p>Click on one of the text labels to toggle the related control:</p>
+        
 
 <form action="demo_form.asp">
   <label for="male">Male</label>
